0b7dfa5ce0015095afdd43fe174c025a8ef08a3d
Gökçen Eraslan <gokcen.eraslan@gmail.com>
2015-11-24 23:13:09 +0100

    Whitespace tidy up

--- ui.R
+++ ui.R
@@ -4,15 +4,15 @@ library(shiny)
 shinyUI(
   #hide top navigation bar
   span(tags$head(tags$style(HTML('.navbar-static-top { display: none; }'))),
-  navbarPage("Misina", id='navbar.panel',
-             tabPanel("Input", htmlOutput('input.page')),
-             
-             tabPanel('Results', htmlOutput('result.page')
-             ),
-             tabPanel('Help', img(src='spinner.gif', id='spinner')
-             ),
-             singleton(
-               tags$head(tags$script(src = "message-handler.js"))
-             )
-  ))
+       navbarPage("Misina", id='navbar.panel',
+                  tabPanel("Input", htmlOutput('input.page')),
+                  
+                  tabPanel('Results', htmlOutput('result.page')
+                  ),
+                  tabPanel('Help', img(src='spinner.gif', id='spinner')
+                  ),
+                  singleton(
+                    tags$head(tags$script(src = "message-handler.js"))
+                  )
+       ))
 )
