c737e74ba05d62008b95ba6d735c13e886f444bd
Gökcen Eraslan <goekcen.eraslan@helmholtz-muenchen.de>
2016-07-03 14:53:30 +0200

    Save mir expression also in JSON format.

--- R/process-mirmine.R
+++ R/process-mirmine.R
@@ -18,9 +18,30 @@ result <- by(mirmine.grouped, mirmine.grouped$mir, function(df){
   }, simplify = F)
 
 df <- data.frame(do.call(rbind, result), stringsAsFactors = F)
-result <- data.frame(mir=rownames(df), mir.median.expression=df[[1]], stringsAsFactors = F)
+result <- data.frame(mir=rownames(df), mirmine.median.expression=df[[1]], stringsAsFactors = F)
 
 saveRDS(result, 'data/processed/mirmine_median_json_2016_april.Rds')
 
 mirmine$group <- as.factor(tissues)
 saveRDS(mirmine, 'data/processed/mirmine_grouped_2016_april.Rds')
+
+result <- by(mirmine.grouped, mirmine.grouped$mir, function(df){ 
+  toJSON(df[, c('expression', 'tissue')])
+  }, simplify = F)
+
+df <- data.frame(do.call(rbind, result), stringsAsFactors = F)
+result <- data.frame(mir=rownames(df), mirmine.median.expression=df[[1]], stringsAsFactors = F)
+
+saveRDS(result, 'data/processed/mirmine_median_json_2016_april.Rds')
+
+
+
+# human readable grouping -------------------------------------------------
+
+result.human <- by(mirmine.grouped, mirmine.grouped$mir, function(df){ 
+  paste(df$tissue, round(df$expression,1), sep = ':', collapse = ',')
+  }, simplify = F)
+
+df.human <- data.frame(do.call(rbind, result.human), stringsAsFactors = F)
+result.human <- data.frame(mir=rownames(df.human), mirmine.median.expression=df.human[[1]], stringsAsFactors = F)
+saveRDS(result.human, 'data/processed/mirmine_median_humanreadable_2016_april.Rds')
