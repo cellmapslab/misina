25d6a7d8deb1e892cef02d484cc5d6fd39a2ac54
Gökcen Eraslan <goekcen.eraslan@helmholtz-muenchen.de>
2016-07-04 13:23:55 +0200

    Print a warning if mir expression doesn't exist

--- render-mirexp.R
+++ render-mirexp.R
@@ -19,8 +19,11 @@ render.mir.expression <- function(mir) {
 
 render.mirmine <- function(x) {
   mirmine <- readRDS('data/processed/mirmine_grouped_2016_april.Rds')
+  df <- subset(mirmine, mir == x)
+  if(nrow(df) == 0) return(h4('Expression info not found.'))
+  
   renderPlot({
-    ggplot(subset(mirmine, mir == x), 
+    ggplot(df, 
            aes(x=group, y=expression)) + 
       geom_boxplot() + 
       geom_point() + 
@@ -32,8 +35,11 @@ render.mirmine <- function(x) {
 
 render.miratlas <- function(x) {
   mirtissueatlas <- readRDS('data/processed/mirtissueatlas_grouped_2016_may.Rds')
+  df <- subset(mirtissueatlas, mir == x)
+  if(nrow(df) == 0) return(h4('Expression info not found.'))
+  
   renderPlot({
-    ggplot(subset(mirtissueatlas, mir == x), aes(x=tissue, y=expression)) + 
+    ggplot(df, aes(x=tissue, y=expression)) + 
       geom_boxplot() + 
       geom_point() + 
       labs(x='Tissue', y='Expression') +
